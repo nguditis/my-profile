17710a3f0a80be0dac19efbd87c3482650a19097
Nathan Guditis <35582385+nguditis@users.noreply.github.com>
2018-01-19 12:58:23 -0500

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -2,3 +2,7 @@
 
 echo "This is a silly script" > /tmp/silly.txt
 
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="sudo apt-get update"))
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="sudo apt-get install -y apache2"))
